f630d9e13eb7c419ff1349a7e6764071b26fe2c8
elenore29 <elenore.nava@gmail.com>
2019-05-02 16:29:28 -0500

    Implementando primer pantalla de la aplicación con el logotipo de burger queen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import logo from './images/logo.png';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Burger Queen</h1>
+        <img className="burger-logo" src={logo} alt="Logo"></img>
       </header>
     </div>
   );
