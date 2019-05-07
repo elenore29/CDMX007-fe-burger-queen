601d77ef6d46d368374d90b0b47e07d8800d49f5
elenore29 <elenore.nava@gmail.com>
2019-05-07 12:59:45 -0500

    Re escribiendo el menu.json y haciendo el map en cada componente para pintar el menu

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import Client from './components/Client.js';
 import Breakfast from './components/Breakfast.js'; 
-import Hamburgers from './components/hamburgers.js'; 
+import Hamburgers from './components/Hamburgers.js'; 
 import Specifications from './components/Specifications.js'; 
 import Logout from './images/logout-icon.png' 
 
@@ -42,7 +42,7 @@ const AppRouter = () => {
               <Link to="/especificaciones/" className="menu-link">Especificaciones</Link>
             </li>
             <li style={{ listStyleType: "none" }}>
-              <Link to="/" className="menu-link"><img class="logout" src={Logout}></img></Link>
+              <Link to="/" className="menu-link"><img className="logout" src={Logout} alt="Cerrar Sesión"></img></Link>
             </li>
           </ul>
         </nav>
