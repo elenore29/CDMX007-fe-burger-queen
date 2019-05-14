f35d3dd26fedff4f7558f142ae6d0e625595167b
elenore29 <elenore.nava@gmail.com>
2019-05-14 10:28:40 -0500

    Subiendo cambios con error en el imput

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,38 +1,17 @@
 import React, {Component} from 'react';
 import './Client.css';
 import logo from '../images/logo.png';
+import Input from './Input.js'; 
 
 class Client extends Component {
-
-  constructor(props) {
-    super(props)
-      this.state = {
-        text: ''
-      }
-      this.handleInput = this.handleInput.bind(this)
-  };
-    
-  handleInput(event){
-    const valor = event.target.value;
-
-      this.setState({
-        text: valor
-      });
-/*       this.props.handleClientName(this.state.text) 
- */  }
-
   render() {
+    console.log('client props', this.props); 
     return ( 
       <div className="App">
         <header className="App-header">
           <img className="burger-logo" src={logo} alt="Logo"></img>
           <h1>NOMBRE</h1>
-          <div>
-            <input handleClientName={this.props.handleClientName} className="client-name" placeholder=" Nombre del cliente" type="text" onChange={this.handleInput} value={this.state.text}></input>
-            <div className="client-name2">{this.state.text}</div>
-          </div>
-{/*               <Input handleClientName={this.props.handleClientName}></Input>
- */}            
+          <Input handleClientName={this.props.handleClientName}></Input>            
         </header>
       </div>
     )
