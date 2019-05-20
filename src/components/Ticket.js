6f84ef8bfb02f3740ee161fb76b76bf45a866979
elenore29 <elenore.nava@gmail.com>
2019-05-19 20:41:35 -0500

    Ordenando el codigo

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,30 +1,30 @@
 import React, {Component, Fragment} from 'react';
 import {AppContextConsumer} from '../store/index.js';
-import '../App.css'
+import '../App.css';
 
 class Ticket extends Component{
   render(){
     return ( 
       <AppContextConsumer>
-          {value => {
-            console.log(value.buttonValue)
-    return (
-      <Fragment>
-        <div className="ticket-box">
-          <div className="ticket">Ticket
-          <div className="client-name2">{value.inputValue}</div>
-           <ul>
-            {value.buttonValue.map(element => 
-              <li style={{ listStyleType: "none" }} className="product-list">{element}</li>
-              )}
-            </ul> 
-            <div className="total">$0.00</div>
-          </div>
-        </div>
-      </Fragment> 
+        {value => {
+          return (
+            <Fragment>
+              <div className="ticket-box">
+                <div className="ticket">Ticket
+                  <div className="client-name2">{value.inputValue}</div>
+                  <ul>
+                    {value.buttonValue.map(element => 
+                      <li style={{ listStyleType: "none" }} className="product-list">{element} 
+                      </li>
+                    )}
+                  </ul> 
+                  <div className="total">$0.00</div>
+                </div>
+              </div>
+            </Fragment> 
           )
         }} 
-    </AppContextConsumer>     
+      </AppContextConsumer>     
     ); 
   }
 }
