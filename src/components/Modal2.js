75796baf1d5e29706f252f7448391c38d88945ad
elenore29 <elenore.nava@gmail.com>
2019-05-25 16:40:54 -0500

    Creando modales de confirmar y cancelar orden

---
+++ src/components/Modal2.js
@@ -0,0 +1,32 @@
+import React, {Component, Fragment} from 'react';
+import {AppContextConsumer} from '../store/index.js';
+import '../App.css';
+import cancel from '../images/salir.png';
+
+class ModalToCancel extends Component{
+  render(){
+    return ( 
+      <AppContextConsumer>
+        {value => {
+          return (
+            <Fragment>
+                <div onClick={(e) => { 
+                    if(e.target.className === 'modal-container'){
+                        value.toggleModal();
+                    }
+                }}
+                     className="modal-container">
+                    <div className="modal">
+                        <img src={cancel} alt="Ok"></img>
+                        <p className="modal-title">¡OK! Tu pedido ha sido cancelado.</p>
+                    </div>
+                </div>
+            </Fragment> 
+          )
+        }} 
+      </AppContextConsumer>     
+    ); 
+  }
+}
+
+export default ModalToCancel; 
